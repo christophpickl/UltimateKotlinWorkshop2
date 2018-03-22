3f407ffda7529bed649e1d3de254bc725fad4cc9
christophpickl <christoph.pickl@gmail.com>
2018-03-22 17:52:10 +0100

    refactor main method

--- ultimate_solution/src/main/kotlin/com/github/christophpickl/ultimate2/Ultimate2Application.kt
+++ ultimate_solution/src/main/kotlin/com/github/christophpickl/ultimate2/Ultimate2Application.kt
@@ -4,8 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Ultimate2Application
-
-fun main(args: Array<String>) {
-    runApplication<Ultimate2Application>(*args)
+class Ultimate2Application {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<Ultimate2Application>(*args)
+        }
+    }
 }
