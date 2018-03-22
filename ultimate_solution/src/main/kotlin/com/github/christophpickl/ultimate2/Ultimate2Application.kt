d996c5f1bbdae50e59d067edf22f7e5835b03e72
christophpickl <christoph.pickl@gmail.com>
2018-03-22 17:44:17 +0100

    start.spring.io template (with updated gradle to 4.6)

---
+++ ultimate_solution/src/main/kotlin/com/github/christophpickl/ultimate2/Ultimate2Application.kt
@@ -0,0 +1,11 @@
+package com.github.christophpickl.ultimate2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ultimate2Application
+
+fun main(args: Array<String>) {
+    runApplication<Ultimate2Application>(*args)
+}
