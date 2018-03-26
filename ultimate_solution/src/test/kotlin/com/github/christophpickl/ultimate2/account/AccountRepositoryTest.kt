db4d62848bb7fc753800593762c751fbdb83dcd5
Christoph Pickl <christoph.pickl@erstegroup.com>
2018-03-26 16:26:47 +0200

    minor changes

--- ultimate_solution/src/test/kotlin/com/github/christophpickl/ultimate2/account/AccountRepositoryTest.kt
+++ ultimate_solution/src/test/kotlin/com/github/christophpickl/ultimate2/account/AccountRepositoryTest.kt
@@ -6,10 +6,12 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @DataJpaTest
+@ActiveProfiles("test")
 class AccountRepositoryTest {
 
     @Autowired
