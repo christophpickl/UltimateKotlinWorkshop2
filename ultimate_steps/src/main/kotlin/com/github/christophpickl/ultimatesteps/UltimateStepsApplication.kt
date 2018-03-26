bcbd671eb2f8a2300e683ce98246f88391168e03
Christoph Pickl <christoph.pickl@erstegroup.com>
2018-03-26 17:10:25 +0200

    initial import

---
+++ ultimate_steps/src/main/kotlin/com/github/christophpickl/ultimatesteps/UltimateStepsApplication.kt
@@ -0,0 +1,11 @@
+package com.github.christophpickl.ultimatesteps
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UltimateStepsApplication
+
+fun main(args: Array<String>) {
+    runApplication<UltimateStepsApplication>(*args)
+}
---
+++ ultimate_steps/src/test/kotlin/com/github/christophpickl/ultimatesteps/ping_test.kt
@@ -0,0 +1,27 @@
+package com.github.christophpickl.ultimatesteps
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.RequestEntity
+import org.springframework.test.context.junit4.SpringRunner
+import java.net.URI
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class PingTest {
+
+    @Autowired
+    private lateinit var rest: TestRestTemplate
+
+    @Test
+    fun `When GET ping Then pong text returned`() {
+        val response = rest.exchange(RequestEntity.get(URI.create("/ping")).build(), String::class.java)
+
+        assertThat(response.body).isEqualTo("pong")
+    }
+}
